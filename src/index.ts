25c2f12777317aed42a20f089402a14f388f4fa9
Kellen Piffner <k@8labs.com>
2019-07-22 14:17:13 -0700

    Some initial config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function getContentFiles(path: string) {
+  // just loads up all the possible files
+}
+
+function parseFrontMatter(file: string) {
+  // just pulls that top level token (probably just use a fancy 'end' string for now.  use the open/close strings)
+}
+
+function loadFile(file: string) {
+  // pull all the content
+}
+
+// get files
+// parse headers
+// build out happy hook files to do the rest...
